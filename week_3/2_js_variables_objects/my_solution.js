72fed9f760c730b888b8657bb5cdff8b13e86e5c
blsalcido <bls.salcido@gmail.com>
2014-04-17 20:18:15 -1000

    updating solution
    updating to show second pair session

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -1,4 +1,4 @@
-// I paired [by myself, with:] on this challenge.
+// I paired [by myself] on this challenge.
 
 
 
@@ -121,3 +121,122 @@ assert(
   "9. "
 )
 
+
+
+
+
+
+
+//I also did this challenge again with a partner....Here's the work from our session//
+
+// I paired [with: RJ Bernaldo] on this challenge.
+
+// name: Bridgette Salcido
+// name: RJ Bernaldo 
+
+// Pseudocode
+// #1 declare variable secretNumber and assign an integer to it
+// #2 set secretNumber to 7
+// #3 declare variable "password" and assign a string to it.
+// #4 set "password" to "just open the door"
+// #5 declare allowedIn variable and set it to a boolean
+// #6 set allowedIn to false
+// #7 declare members as an array
+// #8 set first item of members equal to John
+// #9 set fourth element of members array to Mary
+
+// __________________________________________
+// Write your code below.
+
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = 1 > 2;
+var members = [];
+members[0] = "John";
+members[3] = "Mary";
+
+// __________________________________________
+// Refactored Code: Include a refactored version (or justification of your original code) here. 
+
+Nothing to refactor here. All naming conventions are correct and 
+the code passes all specs. The syntax is correct, spacing is proper, 
+and the code is readable. 
+
+
+
+
+// __________________________________________
+// Reflection: Use the reflection guidelines to write a reflection here. 
+// Bridgette's Reflection
+// 
+// Pairing a second time on this challenge seemed a little redundant at first but
+// it actually cleared up the questions I had about the code I wrote on my own. 
+// It turns out that the terms element and index are not interchangeable. Element
+// deals with the thing in the array whereas index refers only to the position of
+// that thing. See my first reflection for clarification on this. 
+
+// __________________________________________
+// Driver Code:  Do not alter code below this line.
+
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+assert(
+  (typeof secretNumber === 'number'),
+  "The value of secretNumber should be a number.",
+  "1. "
+)
+
+assert(
+  secretNumber === 7,
+  "The value of secretNumber should be 7.",
+  "2. "
+)
+
+assert(
+  typeof password === 'string',
+  "The value of password should be a string.",
+  "3. "
+)
+
+assert(
+  password === "just open the door",
+  "The value of password should be 'just open the door'.",
+  "4. "
+)
+
+assert(
+  typeof allowedIn === 'boolean',
+  "The value of allowedIn should be a boolean.",
+  "5. "
+)
+
+assert(
+  allowedIn === false,
+  "The value of allowedIn should be false.",
+  "6. "
+)
+
+assert(
+  members instanceof Array,
+  "The value of members should be an array",
+  "7. "
+)
+
+assert(
+  members[0] === "John",
+  "The first element in the value of members should be 'John'.",
+  "8. "
+)
+
+assert(
+  members[3] === "Mary",
+  "The fourth element in the value of members should be 'Mary'.",
+  "9. "
+)
